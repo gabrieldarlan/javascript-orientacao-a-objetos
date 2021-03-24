d31a0a811da00c6fbc5238bbbef3510138703ef7
gabrieldarlan <gabriel.darlan88@gmail.com>
2021-03-24 19:30:33 -0300

    curso completo

--- ContaCorrente.js
+++ ContaCorrente.js
@@ -1,9 +1,28 @@
+import { Cliente } from './Cliente.js';
 export class ContaCorrente {
+    static numeroDeContas = 0;
     agencia;
-    cliente;
-
+    _cliente;
     _saldo = 0;
 
+    constructor(agencia, cliente) {
+        this.agencia = agencia;
+        this.cliente = cliente;
+        ContaCorrente.numeroDeContas += 1;
+    }
+
+    set cliente(value) {
+        if (value instanceof Cliente) {
+            this._cliente = value;
+        }
+    }
+    get cliente() {
+        return this._cliente;
+    }
+
+    get saldo() {
+        return this._saldo;
+    }
 
     sacar(valor) {
         if (this._saldo >= valor) {
--- index.js
+++ index.js
@@ -1,29 +1,14 @@
 import { Cliente } from './Cliente.js';
 import { ContaCorrente } from './ContaCorrente.js';
 
-const cliente1 = new Cliente();
-cliente1.nome = "Ricardo";
-cliente1.cpf = 11122233309;
-
-
-const cliente2 = new Cliente();
-cliente2.nome = "Alice";
-cliente2.cpf = 88833322209;
-
-const contaCorrenteRicardo = new ContaCorrente();
-contaCorrenteRicardo.agencia = 1001;
-contaCorrenteRicardo.cliente = cliente1;
+const cliente1 = new Cliente("Ricardo", 11122233309);
+const cliente2 = new Cliente("Alice", 88833322209);
+const contaCorrenteRicardo = new ContaCorrente(1001, cliente1);
 contaCorrenteRicardo.depositar(500);
-
-const conta2 = new ContaCorrente();
-conta2.cliente = cliente2;
-conta2.agencia = 102;
-console.log(contaCorrenteRicardo);
-console.log(conta2);
+const conta2 = new ContaCorrente(1002, cliente2);
 
 let valor = 200;
 contaCorrenteRicardo.transferir(valor, conta2);
 
-console.log(`valor: ${valor}`);
-console.log(conta2);
 console.log(contaCorrenteRicardo);
+console.log(ContaCorrente.numeroDeContas);
