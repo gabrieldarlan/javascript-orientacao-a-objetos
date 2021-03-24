3c7662ba4264c3544681441da4c4bfcabf272775
gabrieldarlan <gabriel.darlan88@gmail.com>
2021-03-24 17:55:25 -0300

    capitulo 03 completo

--- index.js
+++ index.js
@@ -1,30 +1,10 @@
-class Cliente {
-    nome;
-    cpf;
-}
-
-class ContaCorrente {
-    agencia;
-    _saldo = 0;
-
-    sacar(valor) {
-        if (this._saldo >= valor) {
-            this._saldo -= valor;
-            return valor;
-        }
-    }
-
-    depositar(valor) {
-        if (valor <= 0) return;
-        this._saldo += valor;
-    }
-
-}
-
+import { Cliente } from './Cliente.js';
+import { ContaCorrente } from './ContaCorrente.js';
 
 const cliente1 = new Cliente();
 cliente1.nome = "Ricardo";
 cliente1.cpf = 11122233309;
+
 
 const cliente2 = new Cliente();
 cliente2.nome = "Alice";
@@ -32,10 +12,18 @@ cliente2.cpf = 88833322209;
 
 const contaCorrenteRicardo = new ContaCorrente();
 contaCorrenteRicardo.agencia = 1001;
+contaCorrenteRicardo.cliente = cliente1;
+contaCorrenteRicardo.depositar(500);
 
-contaCorrenteRicardo.depositar(-100);
-contaCorrenteRicardo.depositar(100);
-contaCorrenteRicardo.depositar(100);
-const valorSacado = contaCorrenteRicardo.sacar(50);
-console.log(valorSacado);
+const conta2 = new ContaCorrente();
+conta2.cliente = cliente2;
+conta2.agencia = 102;
+console.log(contaCorrenteRicardo);
+console.log(conta2);
+
+let valor = 200;
+contaCorrenteRicardo.transferir(valor, conta2);
+
+console.log(`valor: ${valor}`);
+console.log(conta2);
 console.log(contaCorrenteRicardo);
